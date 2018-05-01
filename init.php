619b44178ea8a3cf0ab7de3b304c81972bd7d6a9
Zsolt Balogh <bzz@bzz.hu>
2018-05-01 22:15:42 +0200

    Now I can use sync-ftp with this fw

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 include_once "config.php";
-include_once "config_local.php";
+include_once "../config_local.php";
 
 $conn = mysqli_connect($sql_server, $sql_user, $sql_password, $sql_database, $sql_port);
 if (!$conn) {
